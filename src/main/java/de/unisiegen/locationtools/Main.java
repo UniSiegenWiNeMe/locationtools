aef82d362aed85921c0ac291e4aa7282e29ccf5c
Julian Dax <julian.dax@uni-siegen.de>
2015-07-08 16:57:33 +0200

    Create Main Class

---
+++ src/main/java/de/unisiegen/locationtools/Main.java
@@ -0,0 +1,10 @@
+package de.unisiegen.locationtools;
+
+/**
+ * Created by brodo on 08.07.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Huhu");
+    }
+}
