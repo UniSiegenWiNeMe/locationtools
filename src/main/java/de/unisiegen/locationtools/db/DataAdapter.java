a63aa1d94b263262f538b9b28fcdbae78413c85c
Julian Dax <julian.dax@uni-siegen.de>
2015-07-08 22:14:13 +0200

    clean up in DataAdapter

--- src/main/java/de/unisiegen/locationtools/db/DataAdapter.java
+++ src/main/java/de/unisiegen/locationtools/db/DataAdapter.java
@@ -12,23 +12,20 @@ import java.util.List;
  */
 public interface DataAdapter {
 
-
-
-        public void openDB();
-        public void closeDB();
-        public Location saveLocation(Location loc,long timeStamp);
-        public Location saveLocation(Location loc );
-        public ClusteredLocation saveClusterLocation(Location loc);
-        public ClusteredLocation saveClusterLocation(Location loc, Dataset ds);
-        public ClusteredLocation updateClusteredLocation(ClusteredLocation updatedLoc);
-        public ClusteredLocation updateClusteredLocation(ClusteredLocation updatedLoc, Dataset ds);
-        public void setClusterIDOfLocations(Dataset ds, long id);
-        public void clearLocationHistory(long since, long until);
-        public void clearClusteredLocations(long since, long until);
-        public List<ClusteredLocation> getAllClusterLocs();
-        public List<UserLocation> getAllHistoryLocs(long since, long until, boolean timedescending, boolean onlyUnclustered);
-        public List<UserLocation> getUnclusteredHistoryLocs(long since, long until);
-
+        void openDB();
+        void closeDB();
+        Location saveLocation(Location loc, long timeStamp);
+        Location saveLocation(Location loc);
+        ClusteredLocation saveClusterLocation(Location loc);
+        ClusteredLocation saveClusterLocation(Location loc, Dataset ds);
+        ClusteredLocation updateClusteredLocation(ClusteredLocation updatedLoc);
+        ClusteredLocation updateClusteredLocation(ClusteredLocation updatedLoc, Dataset ds);
+        void setClusterIDOfLocations(Dataset ds, long id);
+        void clearLocationHistory(long since, long until);
+        void clearClusteredLocations(long since, long until);
+        List<ClusteredLocation> getAllClusterLocs();
+        List<UserLocation> getAllHistoryLocs(long since, long until, boolean timedescending, boolean onlyUnclustered);
+        List<UserLocation> getUnclusteredHistoryLocs(long since, long until);
 
 
 }
