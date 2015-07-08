18111ba39d33eb6cba6dc33f7dda361e4030a8d7
Nico Castelli <nico.castelli@uni-siegen.de>
2015-07-08 17:59:48 +0200

    Conflict

--- src/main/java/de/unisiegen/locationtools/cluster/LocationComparator.java
+++ src/main/java/de/unisiegen/locationtools/cluster/LocationComparator.java
@@ -1,11 +1,11 @@
 package de.unisiegen.locationtools.cluster;
+import de.unisiegen.locationtools.Location;
+import de.unisiegen.locationtools.LocationUtils;
+
 import java.util.Comparator;
 
 import java.util.List;
 
-import de.ms.ptenabler.util.Utilities;
-import de.schildbach.pte.LocationUtils;
-import de.schildbach.pte.dto.Location;
 
 public class LocationComparator implements  Comparator<Location>{
 	double lat;
@@ -16,14 +16,16 @@ public class LocationComparator implements  Comparator<Location>{
 	}
 	
 	public int compare(Location lhs, Location rhs) {
+		/* TODO Datenbankzugriff
 		List<Location> history = Utilities.getDataAdapter().getAllLocs();
+
 		if(history.contains(lhs) && !history.contains(rhs)){
 			return -1;
 		}
 		if(!history.contains(lhs) && history.contains(rhs)){
 			return 1;
 		}
-		double distance = LocationUtils.computeDistance(lat, lng, ((double)lhs.lat/100000.0), ((double)lhs.lon/1000000.0)) - LocationUtils.computeDistance(lat, lng, ((double)rhs.lat/1000000.0), ((double)rhs.lon/100000.0));
+		double distance = LocationUtils.computeDistance(lat, lng, ((double) lhs.lat / 100000.0), ((double) lhs.lon / 1000000.0)) - LocationUtils.computeDistance(lat, lng, ((double)rhs.lat/1000000.0), ((double)rhs.lon/100000.0));
 		if(distance >0){
 			return -1;
 		}else{
