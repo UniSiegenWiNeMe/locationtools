3590909ee352c75122a5404a1eb2890c937459d9
Lars Fischer <fischer@wiwi.uni-siegen.de>
2015-07-08 23:39:40 +0200

    irgendwelche Leerzeichen

--- src/main/java/de/unisiegen/locationtools/db/InfluxConnector.java
+++ src/main/java/de/unisiegen/locationtools/db/InfluxConnector.java
@@ -16,6 +16,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
  * Created by lars on 08/07/15.
@@ -136,9 +137,10 @@ public class InfluxConnector implements DataAdapter {
 
     Query query = new Query("SELECT * FROM locations WHERE user = '" + user + "' AND namespace = '" + namespace + "'", dbName);
     QueryResult queryresult = influxDB.query(query);
-   // queryresult.getResults().get(0).getSeries();
-
-    return null;
+    // queryresult.getResults().get(0).getSeries();
+    //queryresult.getResults().stream().map((Function<QueryResult.Result, Void>) System.err::println);
+        System.out.println(queryresult);
+        return null;
     }
 
     @Override
